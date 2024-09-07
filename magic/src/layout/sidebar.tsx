11f4dce926d3c93ff65aa2655be662e1b57408fb
Dominik Sakic <dominik.sakic@yahoo.com>
2024-09-07 09:52:49 +0900

    feat: add wireframe for the chat sidebar

--- magic/src/layout/sidebar.tsx
+++ magic/src/layout/sidebar.tsx
@@ -1,3 +1,52 @@
 export const Sidebar = () => {
-  return <div className="sidebar">h1</div>;
+  return (
+    <div className="sidebar">
+      <div className="main-button-container">
+        <button>Search</button>
+        <button>Create</button>
+      </div>
+      <div className="sidebar-prompt-container">
+        <h2>よく使うプロンプト</h2>
+        <nav>
+          <ul>
+            <li>Email</li>
+            <li>Estimate</li>
+            <li>Why?</li>
+            <li>Example</li>
+            <li>More Examples</li>
+            <li>もっと見る</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="sidebar-chat-container">
+        <div className="time-container">
+          <h2>今日のチャット</h2>
+          <nav>
+            <ul>
+              <li>Email</li>
+              <li>Estimate</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="time-container">
+          <h2>昨日のチャット</h2>
+          <nav>
+            <ul>
+              <li>Email</li>
+              <li>Estimate</li>
+            </ul>
+          </nav>
+        </div>
+        <div className="time-container">
+          <h2>今月のチャット</h2>
+          <nav>
+            <ul>
+              <li>Email</li>
+              <li>Estimate</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
