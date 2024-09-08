a3e587be4473ce0dfe4fee478191983885c4cba6
Dominik Sakic <dominik.sakic@yahoo.com>
2024-09-08 18:25:52 +0900

    feat: add tempalte screen plus css

--- magic/src/components/Template.tsx
+++ magic/src/components/Template.tsx
@@ -1,3 +1,35 @@
 export const Template = () => {
-  return null;
+  return (
+    <div className="template-container">
+      <div></div>
+      <div className="template-input-container">
+        <h2>Template Title</h2>
+        <div className="grid-template-container">
+          <div>
+            Here Comes the Template!
+            <br />
+            Write a message to $[Target]
+            <br />
+            You are a $[Role]
+            <br />
+            $[Text that you want to write him]
+          </div>
+          <div className="input-fields">
+            <input type="text" placeholder="Target" className="input-field" />
+            <input
+              type="text"
+              placeholder="Enter your role"
+              className="input-field"
+            />
+            <textarea
+              placeholder="Enter the text you want to write"
+              className="input-field"
+            />
+          </div>
+        </div>
+        <button className="send-btn">Send</button>
+        <button className="edit-btn">Edit</button>
+      </div>
+    </div>
+  );
 };
