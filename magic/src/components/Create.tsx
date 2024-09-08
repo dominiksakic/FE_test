adb6e3a587d88e59f6622693e81193e904472995
Dominik Sakic <dominik.sakic@yahoo.com>
2024-09-08 15:01:14 +0900

    feat: add css to the tempalte creation screen

--- magic/src/components/Create.tsx
+++ magic/src/components/Create.tsx
@@ -1,19 +1,24 @@
 export const NewPrompt = () => {
   return (
-    <div className="create-container">
-      <div className="form-container">
-        <h1>Template Creation</h1>
-        <div className="input-row">
-          <input
-            type="text"
-            className="title-input"
-            placeholder="Enter title"
-          />
-          <button className="btn">Add</button>
-          <button className="btn">Remove</button>
+    <div className="create-container-0">
+      <div className="create-container">
+        <div className="form-container">
+          <h1>Template Creation</h1>
+          <div className="input-row">
+            <input
+              type="text"
+              className="title-input"
+              placeholder="Enter title"
+            />
+            <button className="btn">Add</button>
+            <button className="btn">Remove</button>
+          </div>
+          <textarea
+            className="text-area"
+            placeholder="Enter text..."
+          ></textarea>
+          <button className="save-btn">Save</button>
         </div>
-        <textarea className="text-area" placeholder="Enter text..."></textarea>
-        <button className="save-btn">Save</button>
       </div>
     </div>
   );
